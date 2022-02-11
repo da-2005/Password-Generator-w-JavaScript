de08ce755c201cbe838b73f1c97b598dbeee499a
kdot <alvarezderrick0@gmail.com>
2022-02-10 21:27:55 -0800

    defined variables that make up a password

---
+++ script.js
@@ -0,0 +1,6 @@
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let specialChars = "!@#$^&%*()+=-[]{}|:<>?,.";
+let numbers = "1234567890";
+
+let pwd = "";
