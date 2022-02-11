95be1393585c0abf605aa408787d33ebf52ac43d
kdot <alvarezderrick0@gmail.com>
2022-02-10 21:35:33 -0800

    click on Generate Password button serves up the prompt on the desired length of the password

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ let specialChars = "!@#$^&%*()+=-[]{}|:<>?,.";
 let numbers = "1234567890";
 
 let pwd = "";
+let lowerSelection = false;
+let upperSelection = false;
+let specialSelection = false;
+let numberSelection = false;
+
+function generate() {
+    let confirmLength = "";
+
+    while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+        confirmLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
+        if (confirmLength === null) {
+            break;
+        }
+    }
+}
